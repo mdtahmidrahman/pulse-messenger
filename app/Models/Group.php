ddb7f28788e2da6c455a11ce2d7a6f81abd347f0
amisadman <inboxsadman@gmail.com>
2025-11-16 23:48:21 +0600

    feat: added model fillable and relations

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+    protected $fillable = [
+        'name',
+        'description',
+        'owner_id',
+        'last_message_id',
+    ];
     //
+    public function users(){
+        return $this->belongsToMany(User::class,'group_user');
+    }
+    public function messages(){
+        return $this->hasMany(Message::class);
+    }
+    public function owner(){
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/MessageAttachment.php
+++ app/Models/MessageAttachment.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class MessageAttachment extends Model
 {
     //
+    protected $fillable = [
+        'message_id',
+        'name',
+        'path',
+        'mime',
+        'size',
+    ];
 }
