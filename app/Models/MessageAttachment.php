a14797d352c8b4fac87138411ac2b49330eb54fe
Md. Tahmid Rahman <157820278+mdtahmidrahman@users.noreply.github.com>
2025-12-17 01:09:56 +0600

    feat: added HasFactory trait to model classes and updated DatabaseSeeder for user and conversation creation

--- app/Models/MessageAttachment.php
+++ app/Models/MessageAttachment.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MessageAttachment extends Model
 {
+    use HasFactory;
     //
     protected $fillable = [
         'message_id',
