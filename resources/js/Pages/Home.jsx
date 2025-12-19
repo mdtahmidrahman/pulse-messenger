1f9e435a5e759e14c8c8c056872b64f255b2d547
amisadman <inboxsadman@gmail.com>
2025-12-19 21:10:25 +0600

    feat: basic chat view and state management

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,8 +1,58 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
 import ChatLayout from '@/Layouts/ChatLayout.jsx';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
+import { useEffect, useRef, useState } from 'react';
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/solid';
 
-function Home() {
-    return <>Messages</>;
+function Home({ selectedConversation = null, messages = null }) {
+    const [localMessages, setLocalMessages] = useState([]);
+    const messagesContainerRef = useRef(null);
+
+    useEffect(() => {
+        setLocalMessages(messages ? messages.data.reverse() : []);
+    }, [messages]);
+
+    return (
+        <>
+            {!messages && (
+                <div className="flex flex-col gap-8 justify-center items-center text-center h-full opacity-35">
+                    <div className="text-2xl md:text-4xl p-16 text-slate-200">
+                        Please select conversation to see messages
+                    </div>
+                    <ChatBubbleLeftRightIcon className="w-32 h-32 inline-block" />
+                </div>
+            )}
+            {messages && (
+                <div className="flex flex-col h-full">
+                    {/* Header Placeholder */}
+                    <div className="p-4 border-b border-gray-700">Header</div>
+
+                    <div
+                        ref={messagesContainerRef}
+                        className="flex-1 overflow-y-auto p-5"
+                    >
+                        {/* Messages Placeholder */}
+                        {localMessages.length === 0 && (
+                            <div className="flex justify-center items-center h-full">
+                                <div className="text-lg text-slate-200">
+                                    No messages found
+                                </div>
+                            </div>
+                        )}
+                        {localMessages.length > 0 && (
+                            <div className="flex flex-col gap-2">
+                                {localMessages.map((msg) => (
+                                    <div key={msg.id} className="text-white">{msg.message}</div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
+
+                    {/* Input Placeholder */}
+                    <div className="p-4 border-t border-gray-700">Input</div>
+                </div>
+            )}
+        </>
+    );
 }
 
 Home.layout = (page) => {
