1d5b1abea9c7db26be84da662c41ac3a39fe28e3
mdtahmidrahman <mdtahmidr@gmail.com>
2025-12-26 00:26:02 +0600

    Excluded broadcasting auth from CSRF verification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'broadcasting/auth',
         ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
