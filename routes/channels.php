41bc0ced01fe35da8753e4b97c9eb735dc4b6094
mdtahmidrahman <mdtahmidr@gmail.com>
2026-06-24 02:26:01 +0600

    feat: implemented real-time events for new user registration approvals

--- routes/channels.php
+++ routes/channels.php
@@ -6,10 +6,14 @@ use App\Models\User;
 
 Broadcast::channel('online', function (User $user) { return $user ? new UserResource($user) : null;});
 
-Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, int $userId1, int $userId2) { 
-    return $user->id === $userId1 || $user->id === $userId2 ? $user : null;
+Broadcast::channel('message.user.{userId1}-{userId2}', function (User $user, $userId1, $userId2) { 
+    return $user->id == $userId1 || $user->id == $userId2 ? $user : null;
 });
 
-Broadcast::channel('message.group.{groupId}', function (User $user, int $groupId) { 
+Broadcast::channel('message.group.{groupId}', function (User $user, $groupId) { 
     return $user->groups->contains('id', $groupId) ? $user : null;
+});
+
+Broadcast::channel('admin.notifications', function (User $user) {
+    return (bool) $user->is_admin;
 });
