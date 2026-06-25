93d8b08f5704bba5c470d35d6c815d30b5fdc63a
mdtahmidrahman <mdtahmidr@gmail.com>
2026-06-25 22:36:39 +0600

    Add custom Brevo API mail transport driver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -43,5 +43,51 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             \URL::forceScheme('https');
         }
+
+        // Register custom Brevo API Mail Transport to bypass shared hosting SMTP blocks
+        $this->app->make(\Illuminate\Mail\MailManager::class)->extend('brevo', function () {
+            return new class(new \GuzzleHttp\Client()) extends \Symfony\Component\Mailer\Transport\AbstractTransport {
+                public function __construct(private \GuzzleHttp\Client $client) {
+                    parent::__construct();
+                }
+
+                protected function doSend(\Symfony\Component\Mailer\SentMessage $message): void
+                {
+                    $email = \Symfony\Component\Mime\MessageConverter::toEmail($message->getOriginalMessage());
+                    
+                    $to = [];
+                    foreach ($email->getTo() as $recipient) {
+                        $to[] = ['email' => $recipient->getAddress(), 'name' => $recipient->getName()];
+                    }
+
+                    $from = ['email' => $email->getFrom()[0]->getAddress(), 'name' => $email->getFrom()[0]->getName()];
+                    
+                    $payload = [
+                        'sender' => $from,
+                        'to' => $to,
+                        'subject' => $email->getSubject(),
+                    ];
+
+                    if ($email->getHtmlBody()) {
+                        $payload['htmlContent'] = $email->getHtmlBody();
+                    } else {
+                        $payload['textContent'] = $email->getTextBody();
+                    }
+
+                    $this->client->post('https://api.brevo.com/v3/smtp/email', [
+                        'headers' => [
+                            'api-key' => env('BREVO_API_KEY'),
+                            'Content-Type' => 'application/json',
+                        ],
+                        'json' => $payload,
+                    ]);
+                }
+
+                public function __toString(): string
+                {
+                    return 'brevo';
+                }
+            };
+        });
     }
 }
