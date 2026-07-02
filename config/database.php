ecc9b644dace727ecddef8293c2ed52f64c564c2
mdtahmidrahman <mdtahmidr@gmail.com>
2026-07-02 06:08:13 +0600

    fix: emulated prepared statements for pgsql to support PgBouncer transaction pooling

--- config/database.php
+++ config/database.php
@@ -96,7 +96,9 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'require'),
-            'prepared' => env('DB_PREPARED', false),
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
